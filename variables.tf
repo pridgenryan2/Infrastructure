b55001a1b96e1b336919cad880b6910285e67179
pridgenryan2 <pridgenryan2@gmail.com>
2025-12-22 02:25:29 -0500

    Variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "domain" {
+  type = string
+  description = "El dominio del ambiente correspondiente (ex. work-dev.com, work-tqa.com, work.com)"
+}
+
+variable "environment" {
+  type = string
+  description = "ID del ambiente."
+}
